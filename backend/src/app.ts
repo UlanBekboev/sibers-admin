3f03b51b857fa6140610cc94c10fc063041573e9
Ulan Bekbooev <ubekboouulu@gmail.com>
2025-09-04 17:51:30 +0600

    Загатовка для проекта

---
+++ backend/src/app.ts
@@ -0,0 +1,68 @@
+// src/app.ts
+import express, { Application } from "express";
+import session from "express-session";
+import path, { join } from "path";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { sequelize } from "./config/db.js";
+import exphbs from "express-handlebars";
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app: Application = express();
+
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: join(__dirname, "views/layouts"),
+    partialsDir: join(__dirname, "views/partials"),
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://cdnjs.cloudflare.com"], // если нужны внешние скрипты
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", "data:"],
+    },
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Статика
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Роуты (позже подключим auth, users и т.п.)
+app.get("/", (req, res) => {
+  res.render("index", { title: "Welcome", message: "Hello from Express + TS!" });
+});
+
+// DB connect
+sequelize
+  .authenticate()
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error("DB connection error:", err));
+
+export default app;
---
+++ backend/src/config/db.ts
@@ -0,0 +1,21 @@
+// src/config/db.ts
+import { Sequelize } from "sequelize-typescript";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5433,
+  dialect: "postgres",
+  models: [join(__dirname + "/../models")], // путь к моделям
+  logging: false,
+});
